5f2e3aeb3043d40815f2a4726b808944948dee56
Sebastian Daschner <git@sebastian-daschner.de>
2019-09-22 10:54:50 +0200

    added hello resource

---
+++ src/main/java/io/openliberty/sample/hello/HelloResource.java
@@ -0,0 +1,14 @@
+package io.openliberty.sample.hello;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("hello")
+public class HelloResource {
+
+    @GET
+    public String hello() {
+        return "Hello";
+    }
+
+}
